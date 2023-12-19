815c35832165b0518c6a5503efb02fbf24551fa9
Rafael del Rio <rafaelgdelrio@gmail.com>
2023-12-19 10:50:12 -0300

    Add StopwatchFragment and TimerFragment

---
+++ app/src/main/java/com/rafael/clock/StopwatchFragment.kt
@@ -0,0 +1,17 @@
+package com.rafael.clock
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class StopwatchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_stopwatch, container, false)
+    }
+}
