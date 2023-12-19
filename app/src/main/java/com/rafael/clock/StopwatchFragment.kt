259d95af948bc5ec1c7175b2f11df1f832c22aaa
Rafael del Rio <rafaelgdelrio@gmail.com>
2023-12-19 11:46:29 -0300

    Update StopwatchFragment with ViewBinding

--- app/src/main/java/com/rafael/clock/StopwatchFragment.kt
+++ app/src/main/java/com/rafael/clock/StopwatchFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.rafael.clock.databinding.FragmentStopwatchBinding
 
 class StopwatchFragment : Fragment() {
+    private var _binding: FragmentStopwatchBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_stopwatch, container, false)
+        _binding = FragmentStopwatchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
